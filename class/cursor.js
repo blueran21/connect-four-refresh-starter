c083fc9234d177286bb67f22b5418d462e4d12e8
Ryan <blueran21@gmail.com>
2022-10-13 18:06:48 -0700

    Add files via upload

---
+++ class/cursor.js
@@ -0,0 +1,63 @@
+const Screen = require("./screen");
+
+class Cursor {
+
+  constructor(numRows, numCols) {
+    this.numRows = numRows;
+    this.numCols = numCols;
+
+    this.row = 0;
+    this.col = 0;
+
+    this.gridColor = 'black';
+    this.cursorColor = 'yellow';
+
+  }
+
+  resetBackgroundColor() {
+    Screen.setBackgroundColor(this.row, this.col, this.gridColor);
+  }
+
+  setBackgroundColor() {
+    Screen.setBackgroundColor(this.row, this.col, this.cursorColor);
+    Screen.render();
+  }
+
+  left() {
+    // Move cursor left
+    this.resetBackgroundColor();
+    if (this.col > 0) {
+      this.col -= 1;
+    }
+    this.setBackgroundColor();
+  }
+
+  right = () => {
+    // Move cursor right
+    this.resetBackgroundColor();
+    if (this.col < this.numCols - 1) {
+      this.col += 1;
+    }
+    this.setBackgroundColor();
+  }
+
+  down() {
+    this.resetBackgroundColor();
+    if (this.row < this.numRows - 1) {
+      this.row += 1;
+    }
+    this.setBackgroundColor();
+  }
+
+  up() {
+    this.resetBackgroundColor();
+    if (this.row > 0) {
+      this.row -= 1;
+    }
+    this.setBackgroundColor();
+  }
+
+}
+
+
+module.exports = Cursor;
